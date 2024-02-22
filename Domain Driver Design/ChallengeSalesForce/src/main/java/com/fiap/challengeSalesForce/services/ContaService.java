15f57bd044d68e9a674ed74bea6732a137277339
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-02-22 16:03:45 -0300

    Tratar exception ResourceNotFound

--- Domain Driver Design/ChallengeSalesForce/src/main/java/com/fiap/challengeSalesForce/services/ContaService.java
+++ Domain Driver Design/ChallengeSalesForce/src/main/java/com/fiap/challengeSalesForce/services/ContaService.java
@@ -5,6 +5,7 @@ import com.fiap.challengeSalesForce.entities.Conta;
 import com.fiap.challengeSalesForce.entities.enums.AccountStatus;
 import com.fiap.challengeSalesForce.repositories.ContaRepository;
 import com.fiap.challengeSalesForce.repositories.PessoaRepository;
+import com.fiap.challengeSalesForce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,7 +30,8 @@ public class ContaService {
 
     @Transactional(readOnly = true)
     public ContaDTO findById(Long id) {
-        Conta conta = repository.findById(id).get();
+        Conta conta = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado !!"));
         conta.setUltimoAcesso(LocalDateTime.now());
         return new ContaDTO(repository.save(conta));
     }
@@ -43,18 +45,18 @@ public class ContaService {
     }
 
     @Transactional
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         repository.deleteById(id);
     }
 
-    public ContaDTO update(Long id, ContaDTO contaDTO){
+    public ContaDTO update(Long id, ContaDTO contaDTO) {
         Conta conta = repository.findById(id).get();
         updateDtoToEntity(contaDTO, conta);
         repository.save(conta);
         return new ContaDTO(conta);
     }
 
-    private void copyDtoToEntity(ContaDTO contaDTO, Conta conta){
+    private void copyDtoToEntity(ContaDTO contaDTO, Conta conta) {
         conta.setId(contaDTO.getId());
         conta.setUsuario(contaDTO.getUsuario());
         conta.setEmail(contaDTO.getEmail());
@@ -65,14 +67,14 @@ public class ContaService {
         conta.setPessoa(pessoaRepository.getReferenceById(contaDTO.getPessoaId()));
     }
 
-    private void updateDtoToEntity(ContaDTO contaDTO, Conta conta){
+    private void updateDtoToEntity(ContaDTO contaDTO, Conta conta) {
         if (contaDTO.getUsuario() != null) {
             conta.setUsuario(contaDTO.getUsuario());
         }
-        if (contaDTO.getEmail() != null){
+        if (contaDTO.getEmail() != null) {
             conta.setEmail(contaDTO.getEmail());
         }
-        if (contaDTO.getSenha() != null){
+        if (contaDTO.getSenha() != null) {
             conta.setSenha(contaDTO.getSenha());
         }
         conta.setUltimoAcesso(LocalDateTime.now());
