d9da67c3bc353f48bea06b78bd36cb8c417ba0d6
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-20 08:32:18 -0300

    update DDL and DML, BD

--- Building Relational Database/1TDSPS_2023_CodigoSqlDML.sql
+++ Building Relational Database/1TDSPS_2023_CodigoSqlDML.sql
@@ -19,15 +19,15 @@ DESC TB_SERVICO_CONTA;
 
 -- INSERT TB_EMPRESA
 INSERT INTO TB_EMPRESA (NOME, DEPARTAMENTO, DIVISAO, NUM_FUNCIONARIO, INICIO_JORNADA, FIM_JORNADA) VALUES
-('Tech Solutions', 'TI', 'Desenvolvimento', 100, '08:00:00', '17:00:00');
+('Tech Solutions', 'TI', 'Desenvolvimento', '10 a 100', '08:00:00', '17:00:00');
 INSERT INTO TB_EMPRESA (NOME, DEPARTAMENTO, DIVISAO, NUM_FUNCIONARIO, INICIO_JORNADA, FIM_JORNADA) VALUES
-('Global HR', 'RH', 'Recrutamento', 50, '09:00:00', '18:00:00');
+('Global HR', 'RH', 'Recrutamento', '10 a 50', '09:00:00', '18:00:00');
 INSERT INTO TB_EMPRESA (NOME, DEPARTAMENTO, DIVISAO, NUM_FUNCIONARIO, INICIO_JORNADA, FIM_JORNADA) VALUES
-('FinanceCorp', 'Financeiro', 'Contabilidade', 200, '08:30:00', '17:30:00');
+('FinanceCorp', 'Financeiro', 'Contabilidade', '10 a 200', '08:30:00', '17:30:00');
 INSERT INTO TB_EMPRESA (NOME, DEPARTAMENTO, DIVISAO, NUM_FUNCIONARIO, INICIO_JORNADA, FIM_JORNADA) VALUES
-('MarketGenius', 'Marketing', 'Publicidade', 75, '10:00:00', '19:00:00');
+('MarketGenius', 'Marketing', 'Publicidade', '10 a 75', '10:00:00', '19:00:00');
 INSERT INTO TB_EMPRESA (NOME, DEPARTAMENTO, DIVISAO, NUM_FUNCIONARIO, INICIO_JORNADA, FIM_JORNADA) VALUES
-('LogiTrack', 'Operações', 'Logística', 150, '07:00:00', '16:00:00');
+('LogiTrack', 'Operações', 'Logística', '10 a 150', '07:00:00', '16:00:00');
 
 -- INSERT TB_ENDERECO
 INSERT INTO TB_ENDERECO (RUA, CIDADE, ESTADO, CEP, PAIS) VALUES
@@ -42,16 +42,16 @@ INSERT INTO TB_ENDERECO (RUA, CIDADE, ESTADO, CEP, PAIS) VALUES
 ('Rua Sete de Setembro, 202', 'Porto Alegre', 'RS', '90010-240', 'Brasil');
 
 -- INSERT TB_PESSOA
-INSERT INTO TB_PESSOA (NOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
-('Fernando Almeida', 'Nando', '987654326', '19987654326', 'Analista de Sistemas', '123456794', 1, 1);
-INSERT INTO TB_PESSOA (NOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
-('Luiza Costa', 'Lu', '987654327', '19987654327', 'Gerente de RH', '123456795', 2, 2);
-INSERT INTO TB_PESSOA (NOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
-('Ricardo Neves', 'Rick', '987654328', '19987654328', 'Auditor Financeiro', '123456796', 3, 3);
-INSERT INTO TB_PESSOA (NOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
-('Marina Duarte', 'Mari', '987654329', '19987654329', 'Designer Gráfico', '123456797', 4, 4);
-INSERT INTO TB_PESSOA (NOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
-('Bruno Lima', 'Bruninho', '987654330', '19987654330', 'Coordenador de Logística', '123456798', 5, 5);
+INSERT INTO TB_PESSOA (NOME, SOBRENOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
+('Fernando', 'Almeida', 'Nando', '987654326', '19987654326', 'Analista de Sistemas', '123456794', 1, 1);
+INSERT INTO TB_PESSOA (NOME, SOBRENOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
+('Luiza', 'Costa', 'Lu', '987654327', '19987654327', 'Gerente de RH', '123456795', 2, 2);
+INSERT INTO TB_PESSOA (NOME, SOBRENOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
+('Ricardo', 'Neves', 'Rick', '987654328', '19987654328', 'Auditor Financeiro', '123456796', 3, 3);
+INSERT INTO TB_PESSOA (NOME, SOBRENOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
+('Marina', 'Duarte', 'Mari', '987654329', '19987654329', 'Designer Gráfico', '123456797', 4, 4);
+INSERT INTO TB_PESSOA (NOME, SOBRENOME, APELIDO, TELEFONE, CELULAR, CARGO, RG, ID_ENDERECO, ID_EMPRESA) VALUES
+('Bruno', 'Lima', 'Bruninho', '987654330', '19987654330', 'Coordenador de Logística', '123456798', 5, 5);
 
 -- INSERT TB_PAGAMENTO
 INSERT INTO TB_PAGAMENTO (DATA_PAGAMENTO, VALOR_TOTAL, FORMA_PAGAMENTO, PARCELAS, VALOR_PARCELAS, DESCRICAO, STATUS) VALUES
@@ -66,32 +66,32 @@ INSERT INTO TB_PAGAMENTO (DATA_PAGAMENTO, VALOR_TOTAL, FORMA_PAGAMENTO, PARCELAS
 (TO_DATE('2024-05-25', 'YYYY-MM-DD'), 1200.00, 'Cartão de Débito', 4, 300.00, 'Pagamento de serviços de marketing', 'Pago');
 
 -- INSERT TB_SERVICO
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Customer 360', 'Plataforma de gestão de relacionamento com o cliente.', 'CRM', 1000.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('IA', 'Inteligência artificial para análise de dados e automação de processos.', 'IA', 1500.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Pequenas Empresas', 'Soluções personalizadas para pequenas empresas.', 'SMB', 800.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Vendas', 'Ferramentas para gestão e automação de vendas.', 'CRM', 1200.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Atendimento ao Cliente', 'Plataforma de atendimento ao cliente multicanal.', 'CRM', 1000.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Marketing', 'Ferramentas para automação de marketing e análise de campanhas.', 'Marketing', 1300.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Commerce', 'Soluções para comércio eletrônico e gestão de lojas virtuais.', 'Commerce', 1500.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Data Cloud', 'Armazenamento e análise de grandes volumes de dados na nuvem.', 'Cloud', 2000.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Tableau', 'Plataforma de visualização e análise de dados.', 'Analytics', 1800.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Mulesoft', 'Integração de sistemas e APIs.', 'Integration', 1700.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Slack', 'Ferramenta de comunicação empresarial.', 'Collaboration', 900.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Plataforma Einstein 1', 'Inteligência artificial para análise preditiva e recomendações.', 'AI', 2000.00);
-INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR) VALUES
-('Sustentabilidade', 'Soluções para gestão de sustentabilidade empresarial.', 'Sustainability', 1600.00);
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Customer 360', 'Plataforma de gestão de relacionamento com o cliente.', 'CRM', 1000.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder11.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('IA', 'Inteligência artificial para análise de dados e automação de processos.', 'IA', 1500.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder12.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Pequenas Empresas', 'Soluções personalizadas para pequenas empresas.', 'SMB', 800.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/product-smb-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Vendas', 'Ferramentas para gestão e automação de vendas.', 'CRM', 1200.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/sales-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Atendimento ao Cliente', 'Plataforma de atendimento ao cliente multicanal.', 'CRM', 1000.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/service-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Marketing', 'Ferramentas para automação de marketing e análise de campanhas.', 'Marketing', 1300.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/marketing-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Commerce', 'Soluções para comércio eletrônico e gestão de lojas virtuais.', 'Commerce', 1500.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/commerce-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Data Cloud', 'Armazenamento e análise de grandes volumes de dados na nuvem.', 'Cloud', 2000.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder29.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Tableau', 'Plataforma de visualização e análise de dados.', 'Analytics', 1800.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder1.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Mulesoft', 'Integração de sistemas e APIs.', 'Integration', 1700.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/integration-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Slack', 'Ferramenta de comunicação empresarial.', 'Collaboration', 900.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/placeholder13.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Plataforma Einstein 1', 'Inteligência artificial para análise preditiva e recomendações.', 'AI', 2000.00, 'https://www.salesforce.com/content/dam/web/global/svg-icons/platform-day.svg');
+INSERT INTO TB_SERVICO (NOME, DESCRICAO, CATEGORIA, VALOR, IMG_URL) VALUES
+('Sustentabilidade', 'Soluções para gestão de sustentabilidade empresarial.', 'Sustainability', 1600.00, 'https://www.salesforce.com/content/dam/web/global/icons/product/products-sustainability-day.svg');
 
 
 -- INSERT TB_PEDIDO
